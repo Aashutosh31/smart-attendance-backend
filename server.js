b3c0da12ee49b4ecfb00a6f9b9493a34f5ba5d6f
Aashutosh Bairagi <aashutosh.bairagi05@gmail.com>
2026-07-16 18:30:25 +0530

    feat: add frontend urls in cors

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const connectDB = require('./config/db');
 const path = require('path');
 const { errorHandler } = require('./middleware/errorMiddleware');
 
+const frontendUrls = process.env.FRONTEND_URL 
+  ? process.env.FRONTEND_URL.split(',').map(url => url.trim())
+  : ['http://localhost:5173'];
 
 // --- THE CRITICAL FIX ---
 // This specific path forces Node.js to load the JavaScript-only version.
@@ -43,12 +46,8 @@ app.use('/api/', limiter);
 app.use(express.json({ limit: '5mb' }));
 
 
-// --- Flexible CORS Configuration ---
 const allowedOrigins = [
-  'https://smart-attendance-frontend-seven.vercel.app', // Your main frontend URL
-  // This Regular Expression allows requests from any Vercel preview URL.
-  /https:\/\/smart-attendance-frontend-.*-aashutosh31s-projects\.vercel\.app$/, 
-  'http://localhost:5173' // For local development
+  ...frontendUrls
 ];
 
 const corsOptions = {
